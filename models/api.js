7d868eadc1c43cad9b9932cd2b437af0ec38bbe2
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-04-27 21:25:09 -0500

    add getWithParams

--- models/api.js
+++ models/api.js
@@ -14,6 +14,19 @@ api.get = function(url, cb) {
 	});
 }
 
+api.getWithParams = function(url, params, cb) {
+	var fullUrl = 'http://' + root_url + url;
+	superagent.get(fullUrl)
+		.send(params)
+		.end(function(err, result) {
+			if (err) {
+				cb(err);
+			} else {
+				cb(null, JSON.parse(result.text), result.statusCode);
+			}
+		});
+}
+
 api.post = function(url, params, cb) {
 	var fullUrl = 'http://' + root_url + url;
 	superagent.post(fullUrl).send(params).end(function(err, result) {
